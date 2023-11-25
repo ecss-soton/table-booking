8a4e19f4c29d951b24d1d182701089299d27074f
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2023-11-25 12:16:12 +0000

    Add ticket holders and allow them to log in.

---
+++ prisma/getTicketHolders.ts
@@ -0,0 +1,38 @@
+import prisma from "./client";
+import {createReadStream} from 'fs'
+import {parse} from 'csv-parse'
+
+(async () => {
+    let ticketHolders: {[key: string]: {name: string, sotonId: string, plusOneName?: string}} = {};
+
+    createReadStream("../doorlist.csv")
+        .pipe(parse({ delimiter: ",", from_line: 2 }))
+        .on("data", (row) => {
+            const name = row[0];
+            const buyerName = row[10];
+            const sotonId = row[11].split('@')[0]
+
+            if (row[11].split('@')[1] !== 'soton.ac.uk') {
+                console.error(`Found non soton.ac.uk email address: ${row[11]}`);
+            }
+
+            if (buyerName !== name) {
+                ticketHolders[sotonId] = {
+                    sotonId,
+                    name: buyerName,
+                    plusOneName: name
+                }
+            } else if (!ticketHolders[sotonId]) {
+                ticketHolders[sotonId] = {
+                    sotonId,
+                    name: buyerName,
+                }
+            }
+        })
+        .on("end", async () => {
+            await prisma.ticketHolders.deleteMany()
+            await prisma.ticketHolders.createMany({
+                data: Object.values(ticketHolders)
+            })
+        })
+})()
